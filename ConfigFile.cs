a8a4d3fa9d31f437dd698c16b39bc6f029e85967
tanpro260196 <tncao@sfu.ca>
2019-02-26 23:00:12 -0800

    Add project files.

---
+++ ConfigFile.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace PermanentEvents
+{
+    public class ConfigFile
+    {
+        #region ConfigVars
+
+        public bool sandstorm = true;
+        public bool rain = true;
+        public bool bloodmoon = false;
+        public bool eclipse = false;
+
+        #endregion ConfigVars
+
+        public static ConfigFile Read(string path)
+        {
+            if (!File.Exists(path))
+            {
+                ConfigFile config = new ConfigFile();
+
+                File.WriteAllText(path, JsonConvert.SerializeObject(config, Formatting.Indented));
+                return config;
+            }
+            return JsonConvert.DeserializeObject<ConfigFile>(File.ReadAllText(path));
+        }
+    }
+}
---
+++ PermanentEvents.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TShockAPI;
+using Terraria;
+using TerrariaApi.Server;
+using System.Reflection;
+using System.IO;
+using TShockAPI.Hooks;
+
+namespace PermanentEvents
+{
+    [ApiVersion(2, 1)]
+    public class PermanentEvents : TerrariaPlugin
+    {
+        #region Plugin Info
+        public override string Name => "PermanentEvents";
+        public override string Author => "tanpro260196/BMS";
+        public override string Description => "Make server event permanent!";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+        #endregion
+
+        public ConfigFile Config = new ConfigFile();
+
+        public PermanentEvents(Main game) : base(game)
+        {
+        }
+
+        private void LoadConfig()
+        {
+            string path = Path.Combine(TShock.SavePath, "PermanentEvents.json");
+            Config = ConfigFile.Read(path);
+        }
+
+        public override void Initialize()
+        {
+            LoadConfig();
+            ServerApi.Hooks.GameUpdate.Register(this, OnUpdate);
+            GeneralHooks.ReloadEvent += reloadconfig;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameUpdate.Deregister(this, OnUpdate);
+            }
+            base.Dispose(disposing);
+        }
+
+        private void reloadconfig(ReloadEventArgs args)
+        {
+            string path = Path.Combine(TShock.SavePath, "PermanentEvents.json");
+            Config = ConfigFile.Read(path);
+            args.Player.SendSuccessMessage("[Permanent Events] Reload Complete!");
+        }
+        private void OnUpdate(EventArgs args)
+        {
+            if (Config.bloodmoon && !Main.dayTime && !Config.eclipse && !Main.bloodMoon)
+            {
+                TSPlayer.Server.SetBloodMoon(!Main.bloodMoon);
+                return;
+            }
+            if (Config.eclipse && Main.hardMode && !Config.bloodmoon && !Main.eclipse)
+            {
+                TSPlayer.Server.SetEclipse(!Main.eclipse);
+                return;
+            }
+            if ((!Main.raining) && (Config.rain))
+            {
+                Main.StartRain();
+                return;
+            }
+            if ((!Terraria.GameContent.Events.Sandstorm.Happening) && (Config.sandstorm))
+            {
+                Terraria.GameContent.Events.Sandstorm.StartSandstorm();
+                return;
+            }
+            return;
+        }
+    }
+}
+
+
